ff2c625fef1ceb0d9ed2075a0c475464961c07d9
programmatoroSeduto <test@test.test>
2020-04-23 16:07:00 +0200

    animazione carosello

--- js/carousel.js
+++ js/carousel.js
@@ -1,18 +1,27 @@
+let carousel_elements_list = null;
+let carousel_active_panel = null;
+
 function carouselImage(n)
 {
     //riferimento a tutti i pannelli nel carosello
-    let panels = $(".splash-carousel-content").children();
+    if(carousel_elements_list === null)
+    {
+        carousel_elements_list = $(".splash-carousel-content").children();
+        carousel_active_panel = carousel_elements_list.first();
+    }
     
     //nascondi tutti i pannelli
-    panels.css({"display": "none"});
+    //panels.css({"display": "none"});
+    carousel_active_panel.fadeOut(600).css({"display": "none"});
 
     //visualizza solo il pannello indicato dal numero
-    let elem = panels.first();
-    for(let i=1; i<=panels.length; i++)
+    let elem = carousel_elements_list.first();
+    for(let i=1; i<=carousel_elements_list.length; i++)
     {
         if(i == n)
         {
-            elem.css({"display": "block"});
+            elem.fadeIn(600).css({"display": "block"});
+            carousel_active_panel = elem;
         }
         else
         {
@@ -20,3 +29,4 @@ function carouselImage(n)
         }
     }
 }
+
