255dffa208d01c98f13884ae5c2fd29bf21cd583
programmatoroSeduto <test@test.test>
2020-04-29 08:34:40 +0200

    (in registration.php) aggiornamento del metodo di assegnazione automatica del nickname: ora tutti i nickname automatici terminano con una serie di 4 cifre generate casualmente, per renderli univoci nel database

--- php/registration.php
+++ php/registration.php
@@ -83,6 +83,42 @@ if($table_credenziali->isSetEmail($email))
     die();
 }
 
+//prima di registrare l'utente, gli assegno automaticamente un nickname
+/*
+    per garantire l'unicità del nickname assegnato automaticamente dal sistema,
+    1-  provo prima con nomecognome; se esiste ...
+    2-  provo con nomecognome<codice di 4 cifre>; se esiste...
+    3-  faccio le seguenti prove fin quando non trovo il nickname giusto:
+        a-  aumento di 1 il codice generato (sempre di 4 cifre) per un tot di tentativi; ad ogni tentativo provo il nuovo nick generto; se ancora non va ...
+        b-  genero random il codice e provo; se ancora non va bene, torno al passo a-
+    
+    continuo così fin quando non sono riuscito a trovare il nickname giusto; questo potrebbe portare a qualche problema...
+    ... ma quante persone vuoi che ci siano di nome MarioRossi? ho 8999 codici! è possibile avere 8999 posti occupati? per un sito così piccolo, no...
+*/
+$nickname = '' . $firstname . $lastname;
+if($table_profili->getIdByNickname($nickname) >= 0)
+{
+    $random_code = rand(1000, 9999);
+    $nickname = '' . $firstname . $lastname . $random_code;
+
+    $n_attemps = 5;
+    $counter = $n_attemps;
+    while($table_profili->getIdByNickname($nickname) >= 0)
+    {
+        if($counter > 0)
+        {
+            $random_code = ($random_code == 9999 ? 1000 : $random_code + 1);
+            $counter--;
+        }
+        else
+        {
+            $random_code = rand(1000, 9999);
+            $counter = $n_attemps;
+        }
+        $nickname = '' . $firstname . $lastname . $random_code;
+    }
+}
+
 //registrazione del profilo nel database
 if($table_credenziali->createAccount($email, $hashOfPass) === -1)
 {
@@ -97,8 +133,8 @@ if($id_profilo === -1)
     die("errore nella ricerca dell'id!");
 }
 
-//registra dati di profilo
-$table_profili->createAccount($id_profilo, '' . $firstname . $lastname, $firstname, $lastname);
+//registrazione dei dati di profilo
+$table_profili->createAccount($id_profilo, $nickname, $firstname, $lastname);
 
 //registrazione completata con successo; ora, fai qualcosa
 echo 'registrazione completata.';
