87bb53e180a584040f796283f2cf7e3a4ee3eec2
Kallaari <lorenzo.terranova96@hotmail.com>
2020-05-04 17:30:25 +0200

    Aggiunte immagini per avatar e relative impostazioni su "preset styles". Modifica sul riquadro avatar in "profile settings style"

--- php/db/data_setup/preset_styles.php
+++ php/db/data_setup/preset_styles.php
@@ -1,6 +1,6 @@
 <?php
 
-/* TEMPLATE PER L'NSERIMENTO DI DATI NEL DATABASE
+/* TEMPLATE PER L'INSERIMENTO DI DATI NEL DATABASE
 $data[] = array(
     'path' => '../assets/avatar/ ', 
     'banner' => array(255, 0, 0),
@@ -12,10 +12,207 @@ $data[] = array(
 //path, banner, colore1, colore2
 $data[] = array(
     'path' => '../assets/avatar/fagiolo.png', 
-    'banner' => array(255, 0, 0),
-    'colore1' => array(255, 0, 0),
-    'colore1' => array(255, 0, 0)
+    'banner' => array(20, 86, 3),
+    'colore1' => array(25, 50, 100),
+    'colore2' => array(0, 0, 0)
 );
+
+$data[] = array(
+    'path' => '../assets/avatar/beemo.jpeg', 
+    'banner' => array(175, 149, 13),
+    'colore1' => array(255, 0, 0),
+    'colore2' => array(255, 0, 0)
+);
+
+$data[] = array(
+    'path' => '../assets/avatar/berlusconi.png', 
+    'banner' => array(49, 37, 72),
+    'colore1' => array(67, 31, 154),
+    'colore2' => array(107, 61, 197)
+);
+
+$data[] = array(
+    'path' => '../assets/avatar/bean.png', 
+    'banner' => array(767, 156, 47),
+    'colore1' => array(180, 255, 194),
+    'colore2' => array(3, 43, 0)
+);
+
+$data[] = array(
+    'path' => '../assets/avatar/blu.png', 
+    'banner' => array(80, 63, 162),
+    'colore1' => array(89, 145, 255),
+    'colore2' => array(0, 17, 66)
+);
+
+$data[] = array(
+    'path' => '../assets/avatar/clam.jpg', 
+    'banner' => array(12, 173, 124),
+    'colore1' => array(89, 145, 255),
+    'colore2' => array(0, 17, 66)
+);
+
+$data[] = array(
+    'path' => '../assets/avatar/corn.png', 
+    'banner' => array(17, 134, 6),
+    'colore1' => array(197, 195, 39),
+    'colore2' => array(64, 95, 5)
+);
+
+$data[] = array(
+    'path' => '../assets/avatar/drill.png', 
+    'banner' => array(88, 28, 111),
+    'colore1' => array(68, 68, 68),
+    'colore2' => array(72, 15, 81)
+);
+
+$data[] = array(
+    'path' => '../assets/avatar/fragola.png', 
+    'banner' => array(206, 129, 188),
+    'colore1' => array(216, 19, 19),
+    'colore2' => array(71, 0, 0),
+);
+
+$data[] = array(
+    'path' => '../assets/avatar/aatrox.png', 
+    'banner' => array(110, 0, 0),
+    'colore1' => array(216, 19, 19),
+    'colore2' => array(71, 0, 0),
+);
+
+$data[] = array(
+    'path' => '../assets/avatar/fnc.png', 
+    'banner' => array(241, 158, 52),
+    'colore1' => array(216, 19, 19),
+    'colore2' => array(71, 0, 0),
+);
+
+$data[] = array(
+    'path' => '../assets/avatar/ganesh.jpg', 
+    'banner' => array(0, 99, 3),
+    'colore1' => array(216, 19, 19),
+    'colore2' => array(71, 0, 0),
+);
+
+$data[] = array(
+    'path' => '../assets/avatar/giallo.png', 
+    'banner' => array(165, 137, 0),
+    'colore1' => array(232, 0, 0),
+    'colore2' => array(230, 105, 146),
+);
+
+$data[] = array(
+    'path' => '../assets/avatar/giara.png', 
+    'banner' => array(9, 31, 82),
+    'colore1' => array(0, 134, 128),
+    'colore2' => array(0, 30, 48),
+);
+
+$data[] = array(
+    'path' => '../assets/avatar/jhin.png', 
+    'banner' => array(56, 15, 121),
+    'colore1' => array(0, 134, 128),
+    'colore2' => array(0, 30, 48),
+);
+
+$data[] = array(
+    'path' => '../assets/avatar/koala.png', 
+    'banner' => array(0, 100, 35),
+    'colore1' => array(153, 0, 204),
+    'colore2' => array(0, 0, 0),
+);
+
+$data[] = array(
+    'path' => '../assets/avatar/marsh.png', 
+    'banner' => array(0, 84, 93),
+    'colore1' => array(255, 255, 255),
+    'colore2' => array(157, 95, 161),
+);
+
+$data[] = array(
+    'path' => '../assets/avatar/nero.png', 
+    'banner' => array(184, 63, 103),
+    'colore1' => array(255, 255, 255),
+    'colore2' => array(157, 95, 161),
+);
+
+$data[] = array(
+    'path' => '../assets/avatar/panda.png', 
+    'banner' => array(255, 232, 140),
+    'colore1' => array(255, 0, 0),
+    'colore2' => array(255, 0, 0)
+);
+
+$data[] = array(
+    'path' => '../assets/avatar/quattro.jpg', 
+    'banner' => array(140, 0, 40),
+    'colore1' => array(255, 0, 0),
+    'colore2' => array(255, 0, 0)
+);
+
+$data[] = array(
+    'path' => '../assets/avatar/re.png', 
+    'banner' => array(36, 99, 0),
+    'colore1' => array(105, 105, 105),
+    'colore2' => array(135, 11, 11)
+);
+
+$data[] = array(
+    'path' => '../assets/avatar/ronin.png', 
+    'banner' => array(0, 136, 82),
+    'colore1' => array(239, 26, 26),
+    'colore2' => array(0, 174, 178)
+);
+
+$data[] = array(
+    'path' => '../assets/avatar/rosso.png', 
+    'banner' => array(255, 144, 65),
+    'colore1' => array(95, 0, 0),
+    'colore2' => array(255, 144, 65)
+);
+
+$data[] = array(
+    'path' => '../assets/avatar/spora.png', 
+    'banner' => array(0, 6, 97),
+    'colore1' => array(255, 255, 255),
+    'colore2' => array(255, 141, 249)
+);
+
+$data[] = array(
+    'path' => '../assets/avatar/sub.png', 
+    'banner' => array(80, 0, 100),
+    'colore1' => array(0, 0, 0),
+    'colore2' => array(255, 141, 249)
+);
+
+$data[] = array(
+    'path' => '../assets/avatar/sub.png', 
+    'banner' => array(155, 75, 0),
+    'colore1' => array(45, 189, 30),
+    'colore2' => array(255, 255, 255)
+);
+
+$data[] = array(
+    'path' => '../assets/avatar/verde.png', 
+    'banner' => array(155, 75, 0),
+    'colore1' => array(45, 189, 30),
+    'colore2' => array(255, 255, 255)
+);
+
+$data[] = array(
+    'path' => '../assets/avatar/vola.png', 
+    'banner' => array(60, 130, 130),
+    'colore1' => array(207, 166, 69),
+    'colore2' => array(0, 0, 0)
+);
+
+$data[] = array(
+    'path' => '../assets/avatar/tk.png', 
+    'banner' => array(47, 113, 72),
+    'colore1' => array(207, 166, 69),
+    'colore2' => array(0, 0, 0)
+);
+
 /*
     INSERISCI AL POSTO DI QUESTO COMMENTO tutte le altre ennuple.
 */
