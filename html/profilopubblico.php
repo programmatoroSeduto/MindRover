b6b25ff8ed1c53197357abc1f318a3afa2f9ea2f
Kallaari <lorenzo.terranova96@hotmail.com>
2020-05-18 17:42:42 +0200

    Footer definitivo (il CSS è footer2.css per ora...)

--- html/profilopubblico.php
+++ html/profilopubblico.php
@@ -47,7 +47,7 @@ $time_subscr = 26;
     <link type="text/css" rel="stylesheet" href="../css/utils/clearsheet.css">
     <link type="text/css" rel="stylesheet" href="../css/nav.css">
     <link type="text/css" rel="stylesheet" href="../css/fonts.css">
-    <link type="text/css" rel="stylesheet" href="../css/footer.css">
+    <link type="text/css" rel="stylesheet" href="../css/footer2.css">
     <link type="text/css" rel="stylesheet" href="../css/text-container.css">
     <link type="text/css" rel="stylesheet" href="../css/profile_settings_style.css">
     <link type="text/css" rel="stylesheet" href="../css/modal.css">
@@ -68,7 +68,6 @@ $time_subscr = 26;
             -ms-overflow-style: none;
             scrollbar-width: none; /* Firefox 64 */
         }      
-
 
     </style>
 
@@ -242,7 +241,7 @@ $time_subscr = 26;
         </div>
     </div>
 
-    <!-- Footer -->
+    <!-- Footer
     <div class="profile-footer footer-bg"> 
         <div class="footer-content">
 
@@ -257,7 +256,32 @@ $time_subscr = 26;
             Golarion©, Toad of Duty© sono proprietà intelletuali di Frog Studios, Inc.</span>
             
         </div>
-    </div>
+    </div> -->
+
+    <footer class="flex-rw">
+
+        <section class="footer-social-section flex-rw">
+            <span class="footer-social-overlap footer-social-connect">
+                <img src="../assets/img/logo.png" style="height: 9vh;"> 
+            </span>
+
+            <span class="footer-social-overlap footer-social-icons-wrapper">
+            <a href="https://www.facebook.com/" class="generic-anchor aa" target="_blank" title="Facebook" itemprop="significantLink"><i class="fab fa-facebook-square d"></i></a>
+            <a href="https://twitter.com/explore" class="generic-anchor aa" target="_blank" title="Twitter" itemprop="significantLink"><i class="fab fa-twitter d"></i></a>
+            <a href="http://instagram.it" class="generic-anchor aa" target="_blank" title="Instagram" itemprop="significantLink"><i class="fab fa-instagram d"></i></a>
+            <a href="https://www.youtube.it" class="generic-anchor aa" target="_blank" title="Youtube" itemprop="significantLink"><i class="fab fa-youtube d"></i></a>
+            <a href="../HTML/comingsoon.php" class="generic-anchor aa" target="_blank" title="Email Frog Studios" itemprop="significantLink"><i class="fas fa-envelope d"></i></a>
+            <a href="https://unige.it/it/" class="generic-anchor aa" target="_blank" title="Università degli Studi di Genova" itemprop="significantLink"><i class="fas fa-university d"></i></a>
+        </span>
+    </section>
+    
+    <section class="footer-bottom-section" style="line-height: 0.8em">
+        <div class="footer-bottom-wrapper" style="font-family: ganesh; text-align: center;">   
+            © 2020 Frog Studios, Inc. Tutti i diritti riservati. mindROVER©, Golarion©, Toad of Duty© sono proprietà intelletuali di Frog Studios, Inc.
+        </div>
+    </section>
+
+    </footer>
 
 </body>
 </html>
