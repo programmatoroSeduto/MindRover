324e313f8b22b4e056215eee854967bd6ddfd9fe
programmatoroSeduto <test@test.test>
2020-05-29 14:22:14 +0200

    modifiche minori

--- index2.php
+++ index2.php
@@ -1,3 +1,3 @@
 <?php
-    header('location: ./html/homepage.html');
+    header('location: ./html/homepage.php');
 ?>
--- test.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-//echo time();
-echo strtotime('29 September 1995 12:54:23');
-
-?>
